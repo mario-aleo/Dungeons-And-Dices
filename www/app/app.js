56441099d8ead4779a1ec7dfec6a363b9b6f35f3
Mario Aleo <marioaleo@gmail.com>
2016-02-27 22:56:34 -0300

    Update

--- www/app/app.js
+++ www/app/app.js
@@ -5,7 +5,7 @@ angular.module("ngapp", [ "ui.router", "ngCordova", "ngStorage" ])
 .run(function($rootScope, $cordovaDevice, $cordovaStatusbar){
   document.addEventListener("deviceready", function () {
     $cordovaStatusbar.overlaysWebView(false); // Always Show Status Bar
-    $cordovaStatusbar.styleHex('#E53935'); // Status Bar With Red Color, Using Angular-Material Style
+    $cordovaStatusbar.styleHex('#616161'); // Status Bar With Red Color, Using Angular-Material Style
     window.plugins.orientationLock.lock("portrait");
   }, false);
   /* Hijack Android Back Button (You Can Set Different Functions for Each View by Checking the $state.current)
