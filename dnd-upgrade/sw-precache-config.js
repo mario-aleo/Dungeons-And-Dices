84573c7fe91b642160ea3cfee1667376223d5b99
Mario Aleo <marioaleo@gmail.com>
2016-06-21 14:19:01 -0200

    Making LinqJs workes offline

--- dnd-upgrade/sw-precache-config.js
+++ dnd-upgrade/sw-precache-config.js
@@ -2,7 +2,8 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/bower_components/linqjs/linq.min.js'
   ],
   navigateFallback: '/index.html'
 };
