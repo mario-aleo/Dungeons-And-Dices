20385474486702007f49675b7c3f9ed940cf72ad
Mario Aleo <marioaleo@gmail.com>
2016-09-29 22:07:02 -0300

    Init SnD-Update

--- dnd-upgrade/liveserver.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 8080;
-
-app.use(express.static(__dirname + '/'));
-
-app.listen(port);
-
-console.log('Node Server Express Web API Running on Port: ' + port);
--- dnd-upgrade/sw-precache-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  staticFileGlobs: [
-    '/index.html',
-    '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
-    '/bower_components/linqjs/linq.min.js'
-  ],
-  navigateFallback: '/index.html'
-};
