c7e2961db76b262980d4eeb024d75fd54571acdd
Mario Aleo <marioaleo@gmail.com>
2016-09-29 22:16:31 -0300

    Update

---
+++ sw-precache-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/bower_components/linqjs/linq.min.js'
+  ],
+  navigateFallback: '/index.html'
+};
